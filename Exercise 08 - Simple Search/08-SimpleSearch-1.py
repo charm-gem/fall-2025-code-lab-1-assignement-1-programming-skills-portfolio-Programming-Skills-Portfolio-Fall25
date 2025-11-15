8bf2207a0e0573b6ebbb97e7cbe99dc78c5a39bd
charmina-gem <charmaine.gemdiaz@gmail.com>
2025-11-15 20:38:20 +0400

    Update 08-SimpleSearch-1.py

--- Exercise 08 - Simple Search/08-SimpleSearch-1.py
+++ Exercise 08 - Simple Search/08-SimpleSearch-1.py
@@ -1,25 +1,3 @@
 names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 found = "sam".lower()
 
-
-
-correct_password = 12345
-login = 0
-attempts = 0
-max_attempts = 5
-
-while login == 0 and attempts < max_attempts:
-    password = int(input("Please enter your password: "))
-    if password == correct_password:
-        login = login + 1
-    else:
-        attempts = attempts + 1
-        remain = max_attempts - attempts
-        if remain > 1:
-            print(f"That is incorrect. You have {remain} attempts left.")
-        elif remain == 1:
-            print(f"That is incorrect. You have {remain} attempt left.")
-if login == 1:
-    print ("You entered the correct password.")
-else:
-    print ("You have reached the maximum number of attempts. The authorities have been alerted.")
